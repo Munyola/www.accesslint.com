8f02c9e3e860b87ce1d35d940334f046e32a7d6b
Tyson Gach <tyson@tysongach.com>
2017-08-27 19:19:24 -0400

    WIP

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,4 +1,23 @@
 module ApplicationHelpers
+  def github_app_url
+    "https://github.com/apps/accesslint"
+  end
+
+  def page_title
+    site_name = "AccessLint"
+    site_tagline = "Automated and continuous web accessibility testing"
+
+    if content_for?(:title)
+      [site_name, yield_content(:title)].join(" - ")
+    else
+      [site_name, site_tagline].join(" - ")
+    end
+  end
+
+  def support_email
+    "support@accesslint.com"
+  end
+
   def svg(name)
     root = Middleman::Application.root
     images_path = config[:images_dir]
