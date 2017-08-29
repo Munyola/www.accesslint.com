12f7e2a43f8d48e4c58ce5a62d4e9fe5a05c86d7
Tyson Gach <tyson@tysongach.com>
2017-08-29 09:09:25 -0400

    Add redirects for old pages

--- config.rb
+++ config.rb
@@ -8,6 +8,8 @@ page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
 
+proxy("_redirects", "netlify_redirects", ignore: true)
+
 configure :development do
   activate :livereload do |reload|
     reload.no_swf = true
